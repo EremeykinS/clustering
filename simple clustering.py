96e3567814790c82e2ea7a07bd4af61f6251b35a
EremeykinS <eremeykins@gmail.com>
2015-01-01 23:54:33 +0300

    simple dataset

--- simple clustering.py
+++ simple clustering.py
@@ -7,10 +7,17 @@ cur = con.cursor()
 cur.execute("SELECT * FROM trades")
 con.commit()
 
-next_set=cur.fetchmany(10)
+frame_len=33
+n_f=100
+
+data=[-1 for i in range(0,n_f)]
+
+for i in range(0,n_f):
+	next_set=cur.fetchmany(frame_len)
+	data[i]=[row[1]-next_set[0][1] for row in next_set]
 
 #print(next_set)
 #print([next_set[i][2] for i in range(len(next_set))])
-print([row[1]-next_set[0][1] for row in next_set])
+print(data)
 
 con.close()
